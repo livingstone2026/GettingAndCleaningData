4740bdc402c8dab0dac1bc80f47e4e98f06990e2
livingstone2026 <chaneri@yahoo.com>
2017-12-26 02:23:37 +0800

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,28 @@
 run_analysis <- function(){
+     
      rm(list=ls())
      
+     #0) Set project directory
      #The name of directory for downloading data.  You may change
      projDir <- './run_analy'
      
      #check if the directory exists.  If not, create it
      if(!dir.exists(projDir)) {dir.create(projDir)}
      
-     #Download the zip file
+     
+     #1) Download the zip file
      temp <- paste0(projDir, '/a.zip')
      fileUrl <-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
      download.file(fileUrl, temp)
      
-     #unzip the file
+     
+     #2) unzip the file
      unzip(temp, exdir = projDir)
+     
+ 
+     #3) read the unzipped text files X_train.txt, subject_train.txt, y_train.txt, 
+     #X_test.txt, subject_test.txt, y_test.txt, features.txt and merge them in 
+     #one data set
      
      #create 3 data directories 
      testDir <- paste0(projDir, '/UCI HAR Dataset/test/')
@@ -34,22 +43,26 @@ run_analysis <- function(){
      y_file <- paste0(trainDir,'y_train.txt')
      X_train <- cbind(read.table(y_file),X_train)
      
-     #combine X_test with X_train
+     #Merge X_test with X_train
      X_comb <- rbind(X_test,X_train)
      
      #read features.txt
      featTitles<-read.table(paste0(miscDir,'features.txt'))
      
-     #make.unique adds .n to duplicate column names. 
-     #dplyr select function does not work if a column name is not unique
+     #add unique feature variable names to the top row of merged file
+     #(make.unique adds .n to duplicate column names. 
+     #dplyr select function does not work if a column name is not unique)
      colnames(X_comb) <- 
      c('activity','subject', make.unique(as.vector(featTitles[,2])))
      
-     #select column that the names contain 'activity' or 'subject', or 'mean()', or 'std()'
+     
+     #4) select column that the names contain 'activity' or 'subject', or 'mean()', or 'std()'
      require(dplyr)
      X_selct <- X_comb %>% 
      dplyr::select(grep('activity|subject|mean\\()|std\\()',names(X_comb)))
      
+     
+     #5) Change activity identifier to descriptive text
      #read activity labels file
      actLabel <-read.table(paste0(miscDir,'activity_labels.txt'))
      #replace activity number with lower case descriptive text in X_selct column 1
@@ -59,26 +72,28 @@ run_analysis <- function(){
      #Extract column names 
      nameVec <- names(X_selct)
      
-     #Change column names to descriptive text
-     #1. Replace '-' with '_' from variable names
+     
+     #6) Replace column names containing '-' with '_' and remove '()'
      nameVec <- gsub('\\-','_',nameVec)
-     #2. remove '()'
      nameVec <- gsub('\\()','',nameVec) 
      #nameVec <- gsub('[[:punct:]]','',nameVec)  -  this should work also
      
      #Write the new column names back to X_selct
      names(X_selct) <- nameVec
      
-     #View and save the tidy file
+     
+     #7) View and save the tidy file
      View(X_selct)
      write.csv(X_selct, file = "./data/X_tidy.csv")
 
-     #Calculate means (using dplyr group by & summarise_all, much faster than ver 2)
+     
+     #8) Calculate means (using dplyr group by & summarise_all, much faster than ver 2)
      require(dplyr)
      #remember not to use 'activity' but activity in group_by function
      X_means <- X_selct %>% group_by(activity, subject) %>% summarise_all(funs(mean))
      
-     #View and save the new means file
+     
+     #9) View and save the new means file
      View(X_means)
      write.csv(X_means, file = "./data/X_means.csv")
      
